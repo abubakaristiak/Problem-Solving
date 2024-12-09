033e04f89653f7beead303652cd5d0d33bed9206
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-12-09 14:04:49 +0600

    added Week-07 Day-01 solved 1 problem

---
+++ Week -07/Day -01/B_Petr_and_a_Combination_Lock.cpp
@@ -0,0 +1,59 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2024-12-09 || 12:34:28
+ * * * * File    : B_Petr_and_a_Combination_Lock.cpp
+ */
+
+
+// https://codeforces.com/problemset/problem/1097/B
+// Problem 01;
+
+
+#include<bits/stdc++.h>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+int main()
+{
+    fast();
+    int n; cin >> n;
+    vector<int> v(n);
+    for(int i=0; i<n; i++){
+        cin >> v[i];
+    }
+
+
+    bool track=false;
+    for(int mask=0; mask<(1<<n); mask++){
+        int sum=0;
+        for(int k=0; k<n; k++){
+            if((mask>>k)&1){
+                sum+=v[k];
+            }else{
+                sum-=v[k];
+            }
+        }
+        if(sum%360==0){
+            track=true;
+            break;
+        }
+    }
+    if(track){
+        yes;
+    }else no;
+    return 0;
+
+
+// Alhamdulillah
+}
