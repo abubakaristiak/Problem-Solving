109f890112aa745067a2485af0ce9230ac23bf5f
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-12-20 02:25:03 +0600

    updated file

--- Week -08/Day -03/B_Array_Decrements.cpp
+++ Week -08/Day -03/B_Array_Decrements.cpp
@@ -7,6 +7,12 @@
  */
 
 
+
+// https://codeforces.com/problemset/problem/1690/B
+// Problem 03
+
+
+
 #include<bits/stdc++.h>
 #define ll long long
 #define pi pair<ll, ll>
@@ -23,6 +29,50 @@ using namespace std;
 int main()
 {
     fast();
+    int t; cin >> t;
+    while (t--)
+    {
+        int n; cin >> n;
+        vector<int> arr1(n), arr2(n);
+        for(int i=0; i<n; i++){
+            cin >> arr1[i];
+        }
+        for(int i=0; i<n; i++){
+            cin >> arr2[i];
+        }
+        
+        int diff=-1, zero_diff=-1;
+        bool flag=false;
+
+        for(int i=0; i<n; i++){
+            if(arr1[i]<arr2[i]){
+                flag=true;
+                break;
+            }
+            if(arr2[i]!=0){
+                if(diff==-1){
+                    diff=arr1[i]-arr2[i];
+                }else{
+                    if(arr1[i]-arr2[i] !=diff){
+                        flag=true;
+                        break;
+                    }
+                }
+            }
+            else zero_diff=max(zero_diff, arr1[i]-arr2[i]);
+        }
+
+        if(flag){
+            no; 
+            continue;
+        }
+
+        if(diff==-1 || zero_diff<=diff){
+            yes;
+        }else no;
+
+
+    }
     
     return 0;
 
