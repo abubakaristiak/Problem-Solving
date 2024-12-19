70791922395a057612f8e28326824572dfe42641
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-12-19 23:55:23 +0600

    added Week-08 Day-03 solved 1 problem

---
+++ Week -08/Day -03/B_Array_Decrements.cpp
@@ -0,0 +1,31 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2024-12-19 || 23:54:52
+ * * * * File    : B_Array_Decrements.cpp
+ */
+
+
+#include<bits/stdc++.h>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+int main()
+{
+    fast();
+    
+    return 0;
+
+
+// Alhamdulillah
+}
