1c146080786b16286d68043ca7ff8e10255f35ac
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-12-20 23:43:27 +0600

    added Week-08 Day-04 solved 1 problem

---
+++ Week -08/Day -04/B_3_palindrome.cpp
@@ -0,0 +1,32 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2024-12-20 || 22:59:13
+ * * * * File    : B_3_palindrome.cpp
+ */
+
+
+
+#include<bits/stdc++.h>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+int main()
+{
+    fast();
+    
+    return 0;
+
+
+// Alhamdulillah
+}
