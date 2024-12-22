24e68cb177e08a71a39abb37517ea48b6d407a18
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-12-22 23:24:14 +0600

    added Week-08 Day-05 solved 1 problem

---
+++ Week -08/Day -05/B_Luntik_and_Subsequences.cpp
@@ -0,0 +1,40 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2024-12-22 || 23:23:42
+ * * * * File    : B_Luntik_and_Subsequences.cpp
+ */
+
+
+#include<bits/stdc++.h>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+int main()
+{
+    fast();
+    
+    int t; cin >> t;
+    while (t--)
+    {
+        int n; cin >> n; 
+        vector<int> v(n);
+    }
+    
+    
+
+    return 0;
+
+
+// Alhamdulillah
+}
