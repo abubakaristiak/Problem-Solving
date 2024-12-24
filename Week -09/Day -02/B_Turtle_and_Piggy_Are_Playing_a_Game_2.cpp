8f7421614a860da34f26b07dd5ddc2207186fc23
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-12-24 23:53:52 +0600

    added Week-09 Day-02 solved 1 problem

---
+++ Week -09/Day -02/B_Turtle_and_Piggy_Are_Playing_a_Game_2.cpp
@@ -0,0 +1,46 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2024-12-24 || 23:47:34
+ * * * * File    : B_Turtle_and_Piggy_Are_Playing_a_Game_2.cpp
+ */
+
+
+
+
+#include<bits/stdc++.h>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+int main()
+{
+    fast();
+    int t; cin >> t;
+    while (t--)
+    {
+        int n; cin >> n;
+        vector<int> v(n);
+
+        for(int i=1; i<=n; i++){
+            cin >> v[i];
+        }
+
+        asort(v);
+        cout << v[n/2+1] << endl;
+    }
+    
+    return 0;
+
+
+// Alhamdulillah
+}
