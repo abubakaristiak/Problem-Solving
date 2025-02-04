e1370b57255d3f35c1ac83a6faf55f8719a2d6ee
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-02-04 23:49:39 +0600

    added new files

---
+++ Practise Contest/Contest 12 - Div. 4/Minimum_Wage.cpp
@@ -0,0 +1,46 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-02-04 || 23:22:05
+ * * * * File    : Minimum_Wage.cpp
+ */
+
+
+
+#include<bits/stdc++.h>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+
+void solve()
+{
+    int n; cin >> n;
+    if(n<=11){
+        no;
+    }else yes;
+}
+
+
+int main()
+{
+    fast();
+    int t=1;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+
+
+// Alhamdulillah
+}
