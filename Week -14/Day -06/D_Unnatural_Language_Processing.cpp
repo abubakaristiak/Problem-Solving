f74a01d9c5260471e64200ed5f335b81f9738773
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-02-11 23:38:47 +0600

    added Week-14 Day-06 solved 1 problem

---
+++ Week -14/Day -06/D_Unnatural_Language_Processing.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve()
+{
+    int n; cin >>  n;
+    string s; cin >> s;
+    for(int i=0; i<n; i++){
+        cout << s[i];
+        if((s[i+2]== 'a' || s[i+2]=='e') && (i+2<=n)){
+            cout << ".";
+        }
+    }
+    cout << endl;
+}
+
+
+int main()
+{
+    fast();
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+
+
+// Alhamdulillah
+}
