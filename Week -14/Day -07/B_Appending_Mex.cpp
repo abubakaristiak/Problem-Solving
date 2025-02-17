95fabebae7adc33d8ad60c0bb4802e948b6ce6b5
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-02-17 23:30:20 +0600

    updated previous problem

---
+++ Week -14/Day -07/B_Appending_Mex.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve()
+{
+    ll n; cin >> n;
+    ll mx=-1;
+    ll x, flag=0;
+    for(int i=0; i<n; i++){
+        cin >> x;
+        if(x>mx+1){
+            cout << i+1;
+            flag=1;
+            break;
+        }
+        mx=max(mx, x);
+    }
+    if(flag==0){
+        cout << -1;
+    }
+}
+
+
+int main()
+{
+    fast();
+    int t=1;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+
+
+// Alhamdulillah
+}
