a12b7d66ad597b82e773e4412aebeb97b5b1e503
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-03-05 23:29:09 +0600

    added Week-15 Day-06 solved 1 problem

---
+++ Week -15/Day -06/D_Same_Differences.cpp
@@ -0,0 +1,62 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-03-05 || 23:16:45
+ * * * * File    : D_Same_Differences.cpp
+*/
+
+
+// https://codeforces.com/problemset/problem/1520/D
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve()
+{
+    ll n; cin >> n;
+    map<ll, ll> mp;
+    for(ll i=1; i<=n; i++){
+        ll x; cin >> x;
+        x-=i;
+        mp[x]++;
+    }
+    ll ans=0;
+    for(auto it:mp){
+        ll cnt=it.second;
+        ans+=(cnt*(cnt-1))/2;
+    }
+    cout << ans << endl;
+}
+
+
+int main()
+{
+    fast();
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+
+
+// Alhamdulillah
+}
